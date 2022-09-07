b857f1f4121309065e26d61ddee9f7d1604d8fae
Alif Wibowo <alif.likes.coding@gmail.com>
2022-09-07 16:18:43 +0700

    Add text variables

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Varibales */
+/* Variables */
+/* General Variables */
 let computerChoice = {Value: ""};
 let computerChoiceInt = 0;
 let playerChoice;
@@ -8,3 +9,15 @@ const selectors = document.querySelectorAll('.selector');
 let computerScore = 0;
 let playerScore = 0;
 
+/* Text Variables */
+const player = document.querySelector("#player-score");
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector('#computer-score');
+computer.textContent = `Computer Score: ${computerScore}`;
+
+const winMessage = document.querySelector('#win-message');
+winMessage.textContent = `First to 3 wins!`;
+
+const luckMessage = document.querySelector('#luck-message');
+luckMessage.textContent = `Good Luck!!!`;
