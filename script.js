1e903f0ec22ba142e48d77fd40573ad874bd0a07
Alif Wibowo <alif.likes.coding@gmail.com>
2022-09-07 16:06:02 +0700

    Make JS variables
    Made some JavaScript variables for the game like player choice,score, etc..

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Varibales */
+let computerChoice = {Value: ""};
+let computerChoiceInt = 0;
+let playerChoice;
+let playerChoiceInt = 0;
+const selectors = document.querySelectorAll('.selector');
+
+let computerScore = 0;
+let playerScore = 0;
+
