ce4930b429b5294f0c0ccb48df11410ef5977557
Alif Wibowo <alif.likes.coding@gmail.com>
2022-09-16 15:47:16 +0700

    Add game JavaScript variables
    Added (initialized / declared) some JavaScript variables for the game like player choice, computer choice, etc...

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Variables */
+// moves
+let elephant = document.querySelector('#elephant');
+let ant = document.querySelector('#ant');
+let human = document.querySelector('#human');
+
+// game score
+let score = 0;
+
+// player choice
+let playerChoice;
+// computer choice
+let computerChoice;
