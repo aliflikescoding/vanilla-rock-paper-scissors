eb7eed984b15698f01f99df3a9ae6d436f28308f
Alif Wibowo <alif.likes.coding@gmail.com>
2022-09-19 08:55:08 +0700

    Add scoring logic
    Added the logic to know if the player or the computer won depending on that the score will change

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let ant = document.querySelector('#ant');
 let human = document.querySelector('#human');
 
 // text
-let roundText = document.querySelector('#win-message');
+let winText = document.querySelector('#win-message');
 let playerScText = document.querySelector('#player-score');
 let computerScText = document.querySelector('#computer-score');
 let luckText = document.querySelector('#luck-message');
@@ -13,7 +13,7 @@ let luckText = document.querySelector('#luck-message');
 // count variables
 let playerScore = 0;
 let computerScore = 0;
-let rounds = 0;
+let wins = 0;
 
 // game choice
 let playerChoice;
@@ -42,5 +42,51 @@ human.addEventListener('click', () => {
 
 // event function
 function action(selected) {
-    
+    playerChoice = selected;
+    computerChoice = getComputerChoice();
+    if (playerChoice == computerChoice) {
+        winText.textContent = `It's a draw, select again`;
+    }
+    else if (playerChoice === 1 && computerChoice === 3) {
+        playerScore++;
+        playerScText.textContent = `Player Score: ${playerScore}`;
+        computerScText.textContent = `Computer Score: ${computerScore}`;
+        winText.textContent = `Player wins this round!!!`;
+        wins++;
+    }
+    else if (playerChoice === 1 && computerChoice === 2) {
+        computerScore++;
+        playerScText.textContent = `Player Score: ${playerScore}`;
+        computerScText.textContent = `Computer Score: ${computerScore}`;
+        winText.textContent = `Computer wins this round!!!`;
+        wins++;
+    }
+    else if (playerChoice === 2 && computerChoice === 1) {
+        playerScore++;
+        playerScText.textContent = `Player Score: ${playerScore}`;
+        computerScText.textContent = `Computer Score: ${computerScore}`;
+        winText.textContent = `Player wins this round!!!`;
+        wins++;
+    }
+    else if (playerChoice === 2 && computerChoice === 3) {
+        computerScore++;
+        playerScText.textContent = `Player Score: ${playerScore}`;
+        computerScText.textContent = `Computer Score: ${computerScore}`;
+        winText.textContent = `Computer wins this round!!!`;
+        wins++;
+    }
+    else if (playerChoice === 3 && computerChoice === 2) {
+        playerScore++;
+        playerScText.textContent = `Player Score: ${playerScore}`;
+        computerScText.textContent = `Computer Score: ${computerScore}`;
+        winText.textContent = `Player wins this round!!!`;
+        wins++;
+    }
+    else if (playerChoice === 3 && computerChoice === 1) {
+        computerScore++;
+        playerScText.textContent = `Player Score: ${playerScore}`;
+        computerScText.textContent = `Computer Score: ${computerScore}`;
+        winText.textContent = `Computer wins this round!!!`;
+        wins++;
+    }
 }
